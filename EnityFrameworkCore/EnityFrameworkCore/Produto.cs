63e73f07109b56273f6e88453008dcaf5a586b26
cpsilva <caiopirees@gmail.com>
2018-04-21 19:25:33 -0300

    Commit inicial, com exemplo de acesso a dados com ADO.NET sera alterado para o entity na proxima aula

---
+++ EnityFrameworkCore/EnityFrameworkCore/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace EnityFrameworkCore
+{
+    internal class Produto
+    {
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
---
+++ EnityFrameworkCore/EnityFrameworkCore/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EnityFrameworkCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GravarUsandoAdoNet();
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
